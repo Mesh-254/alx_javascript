53e747625b5cb382c4598a491530b4513a51aebb
Mesh-254 <meshack1995@gmail.com>
2023-09-13 07:32:36 +0300

    script that gets the contents of a webpage and stores it in a file.

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -6,4 +6,4 @@ const fs = require('fs');
 
 const url = process.argv[2];
 
-request.get(url, { encoding: 'utf-8' }).pipe(fs.createWriteStream(`${process.argv[3]}`));
+request.get(url).pipe(fs.createWriteStream(`${process.argv[3]}`, 'utf-8'));
