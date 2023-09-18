08fff692354793c9602fb4851093ab509873dc22
Mesh-254 <meshack1995@gmail.com>
2023-09-18 12:52:02 +0300

    Private methods with closure

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+const studentHogwarts = function () {
+  let privateScore = 0;
+  let name = null;
+  // private method changescoreby which takes points argument
+  function changeScoreBy (points) {
+    privateScore += points;
+  }
+
+  return {
+    // public method setName to set the private variable name
+    setName: function (newName) {
+      name = newName;
+    },
+    // public method rewardStudent that calls the method changeScoreBy with 1
+    rewardStudent: function () {
+      changeScoreBy(1);
+    },
+    // public method penalizeStudent, that calls the method changeScoreBy with -1
+    penalizeStudent: function () {
+      changeScoreBy(-1);
+    },
+    // public method getScore that returns name and score
+    getScore: () => {
+      console.log(name + ': ' + privateScore);
+    }
+  };
+};
+
+const harry = studentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.getScore();
+
+const draco = studentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.getScore();
